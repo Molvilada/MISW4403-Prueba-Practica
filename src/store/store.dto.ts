57855ca7855684744d3c9f75cb42dc6389e7b829
Laura Molina <ld.molina11@uniandes.edu.co>
2024-09-13 23:41:28 -0500

    :sparkles: store controller and dto

---
+++ src/store/store.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsNumber, IsString, Matches } from 'class-validator';
+import { INCORRECT_CITY_MSG } from './constants';
+
+export class StoreDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Matches(/^[A-Z]{3}$/, {
+    message: INCORRECT_CITY_MSG,
+  })
+  readonly city: Number;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly address: string;
+}
