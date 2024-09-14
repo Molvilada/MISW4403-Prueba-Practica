743955bf29b5e55c41b783855165e92e5571824e
Laura Molina <ld.molina11@uniandes.edu.co>
2024-09-13 20:31:58 -0500

    :sparkles: product service

---
+++ src/product/constants.ts
@@ -0,0 +1,5 @@
+export const PRODUCT_TYPES = ['Perecedero', 'No perecedero'];
+export const Messages = {
+  NOT_FOUND_MSG: 'The product with the given id was not found',
+  INCORRECT_TYPE_MSG: `The product type is incorrect, it must be: ${PRODUCT_TYPES.join(',')}`,
+};
---
+++ src/product/product.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from './product.entity';
+import { Repository } from 'typeorm';
+import {
+  BusinessError,
+  BusinessLogicException,
+} from 'src/shared/errors/business-errors';
+import { Messages, PRODUCT_TYPES } from './constants';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>,
+  ) {}
+
+  async findAll(): Promise<ProductEntity[]> {
+    return await this.productRepository.find({
+      relations: ['stores'],
+    });
+  }
+
+  async findOne(id: string): Promise<ProductEntity> {
+    const product: ProductEntity = await this.productRepository.findOne({
+      where: { id },
+      relations: ['stores'],
+    });
+    if (!product)
+      throw new BusinessLogicException(
+        Messages.NOT_FOUND_MSG,
+        BusinessError.NOT_FOUND,
+      );
+
+    return product;
+  }
+
+  async create(product: ProductEntity): Promise<ProductEntity> {
+    if (!PRODUCT_TYPES.includes(product.type))
+      throw new BusinessLogicException(
+        Messages.INCORRECT_TYPE_MSG,
+        BusinessError.BAD_REQUEST,
+      );
+    return await this.productRepository.save(product);
+  }
+
+  async update(id: string, product: ProductEntity): Promise<ProductEntity> {
+    const persistedProduct: ProductEntity =
+      await this.productRepository.findOne({
+        where: { id },
+      });
+
+    if (!persistedProduct)
+      throw new BusinessLogicException(
+        Messages.NOT_FOUND_MSG,
+        BusinessError.NOT_FOUND,
+      );
+
+    if (!PRODUCT_TYPES.includes(product.type))
+      throw new BusinessLogicException(
+        Messages.INCORRECT_TYPE_MSG,
+        BusinessError.BAD_REQUEST,
+      );
+    product.id = id;
+
+    return await this.productRepository.save(product);
+  }
+
+  async delete(id: string) {
+    const product: ProductEntity = await this.productRepository.findOne({
+      where: { id },
+    });
+    if (!product)
+      throw new BusinessLogicException(
+        Messages.NOT_FOUND_MSG,
+        BusinessError.NOT_FOUND,
+      );
+
+    await this.productRepository.remove(product);
+  }
+}
